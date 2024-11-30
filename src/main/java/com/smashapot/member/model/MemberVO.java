c7d44c2e6b84c0bc4ee08c99f28fa269426c3de5
Jarvis Lee <John0988721002@gmail.com>
2024-11-30 14:50:45 +0800

    first commit

---
+++ src/main/java/com/smashapot/member/model/MemberVO.java
@@ -0,0 +1,23 @@
+package com.smashapot.member.model;
+
+public class MemberVO {
+	private Integer memId;
+	private String memName;
+	
+	
+	public Integer getMemId() {
+		return memId;
+	}
+	public void setMemId(Integer memId) {
+		this.memId = memId;
+	}
+	public String getMemName() {
+		return memName;
+	}
+	public void setMemName(String memName) {
+		this.memName = memName;
+	}
+	
+	
+
+}
